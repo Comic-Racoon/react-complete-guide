ddbfe33e97af97e6e29e7b048fe8603ff4333bbc
Rahul Mhatre <67099690+Comic-Racoon@users.noreply.github.com>
2023-05-03 00:13:20 +0530

    updated files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import Person from "./Person/Person";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hi I am a React App</h1>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi I am a React App</h1>
+        <Person />
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,5 @@
+import React from "react";
+const person = () => {
+  return <div> I am a person </div>;
+};
+export default person;
